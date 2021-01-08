4bdee374a4bb690b1ee2fe0b0e0590cd4cbe7c4f
Pawel <pnagorz@onet.pl>
2021-01-08 21:26:05 +0100

    OAuth2 with GitHub from backend

--- src/main/java/com/example/pawel/expense/config/SecurityConfiguration.java
+++ src/main/java/com/example/pawel/expense/config/SecurityConfiguration.java
@@ -1,14 +1,43 @@
 package com.example.pawel.expense.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.client.test.OAuth2ContextConfiguration.Password;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	public void configuration(HttpSecurity http) throws Exception {
-		http.antMatcher("/**").authorizeRequests()
-			.antMatchers("/").permitAll()
-			.anyRequest().authenticated()
-			.and()
-			.oauth2Login();
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails userDetails = User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("user1")
+				.roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(userDetails);
 	}
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("user")
+		.password("pass")
+		.roles("USER");
+	}
+	
+	public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers("/").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .oauth2Login()
+        .and()
+        .logout().logoutSuccessUrl("/");
+	}
+	
 }
--- src/main/java/com/example/pawel/expense/controller/ExpenseController.java
+++ src/main/java/com/example/pawel/expense/controller/ExpenseController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +25,7 @@ import com.example.pawel.expense.repository.ExpenseRepository;
 
 @RestController
 @RequestMapping("/api")
+@Controller
 public class ExpenseController {
 
 	private ExpenseRepository expenseRepository;
