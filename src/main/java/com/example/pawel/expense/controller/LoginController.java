bab0f1b83f41282ce35ae25bf282dd98182325c2
Pawel <pnagorz@onet.pl>
2021-03-15 14:29:17 +0100

    First steps of registation

--- src/main/java/com/example/pawel/expense/controller/LoginController.java
+++ src/main/java/com/example/pawel/expense/controller/LoginController.java
@@ -1,15 +1,28 @@
 package com.example.pawel.expense.controller;
 
+import com.example.pawel.expense.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
-	
-	@GetMapping("/login")
-	public String getLoginPage(Model model) {
-		return "login";
+
+	private UserService userService;
+
+	@RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+	public ModelAndView login() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("login");
+		return modelAndView;
 	}
+
+//	@GetMapping("/login")						Old login page
+//	public String getLoginPage(Model model) {
+//		return "login";
+//	}
 
 }
