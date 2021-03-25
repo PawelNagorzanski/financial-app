39f912716646556aadca97856cb61262dedfd9bc
Pawel <pnagorz@onet.pl>
2021-03-25 22:09:08 +0100

    POST method for register and visual updates

--- src/main/java/com/example/pawel/expense/controller/LoginController.java
+++ src/main/java/com/example/pawel/expense/controller/LoginController.java
@@ -4,10 +4,13 @@ import com.example.pawel.expense.model.User;
 import com.example.pawel.expense.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class LoginController {
@@ -29,6 +32,26 @@ public class LoginController {
 		modelAndView.setViewName("register");
 		return modelAndView;
 	}
+
+	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+	public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+		ModelAndView modelAndView = new ModelAndView();
+		User userExist = userService.findUserByUserName(user.getUserName());
+		if (userExist != null) {
+			bindingResult.rejectValue("userName", "error.user", "Account with this username already exist");
+		}
+		if (bindingResult.hasErrors()) {
+			modelAndView.setViewName("registration");
+		} else {
+			userService.saveUser(user);
+			modelAndView.addObject("succesMessage", "User registered successfully");
+			modelAndView.addObject("user", new User());
+			modelAndView.setViewName("registration");
+		}
+
+		return modelAndView;
+	}
+
 
 //	@GetMapping("/login")						Old login page
 //	public String getLoginPage(Model model) {
