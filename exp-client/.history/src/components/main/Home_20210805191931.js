121250bfecddc9bfe77664fffc58cc3c220251ce
Pawel <pnagorz@onet.pl>
2021-09-02 19:54:13 +0200

    Deploy

--- exp-client/.history/src/components/main/Home_20210805191931.js
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <>
-      <p>WElcome</p>
-    </>
-  )
-}
-
-export default Home
--- exp-client/src/components/main/Alert.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Alert = ({ text }) => {
-return (
-  <div style={{backgroundColor: "red"}}>{text}</div>
-  )
-}
-
-export default Alert;
